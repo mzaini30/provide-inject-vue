f8b8c987c18577a5f5d332260a2fe65043d31beb
mzaini30 <muhzaini30@gmail.com>
2021-07-16 15:21:47 +0800

    upload

--- _site/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <router-view></router-view>
-  <ReloadPrompt/>
-</template>
-
-<script setup>
-	import ReloadPrompt from './ReloadPrompt.vue'
-  provide('counter', ref(0))
-</script>
--- _site/src/pages/TambahCounter.vue
+++
@@ -1,8 +0,0 @@
-<script setup="">
-	const counter = inject('counter')
-</script>
-
-<template>
-	<button @click='counter++'>Tambah</button>
-	<button @click='counter--'>Kurangi</button>
-</template>
